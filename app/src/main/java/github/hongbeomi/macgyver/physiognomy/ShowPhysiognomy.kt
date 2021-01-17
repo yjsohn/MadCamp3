16fadee2eaed87c242d60590ceb603238e2cac45
yjsohn <panda_8@naver.com>
2021-01-18 01:13:34 +0900

    Calculate Proportion

--- app/src/main/java/github/hongbeomi/macgyver/physiognomy/ShowPhysiognomy.kt
+++ app/src/main/java/github/hongbeomi/macgyver/physiognomy/ShowPhysiognomy.kt
@@ -9,9 +9,16 @@ class ShowPhysiognomy : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_physiognomy)
         var a = intent.getStringExtra("src")
+        var top = intent.getIntExtra("TOP", 0)
         img.setImageURI(a.toUri())
-        var f : Features = intent.getSerializableExtra("FEATURE") as Features
-        feature_tv.setText(f.nose_bridge[0].toString())
+        var feature : Features = intent.getSerializableExtra("FEATURE") as Features
+
+
+        //비율 계산
+        feature = feature.calcFeature()
+        var proportion = (feature.lipLength / feature.faceWidth) * 100
+        feature_tv.setText("face_width: " + feature.faceWidth + ", lip_len: " + feature.lipLength.toString() + ", lip_upper_th: " + feature.upperLipThickness + ", lip_lower_th: " + feature.lowerLipThickness +
+                "\nproportion: " + proportion)
 
     }
 }
